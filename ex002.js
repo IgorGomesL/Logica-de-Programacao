a7c4090ed6cfebd87ed69b4dd00d6894995614ed
Igor Gomes de Lima <gomesigor335@gmail.com>
2026-02-18 17:01:00 -0300

    Update ex002.js

---
+++ ex002.js
@@ -0,0 +1,6 @@
+Calcule o dobro e o triplo de um número digitado.
+
+let numero = 10;
+let dobro = numero * 2;
+let triplo = numero * 3;
+console.log(`O número digitado é de: ${numero}, o dobro dele é de: ${dobro}, o triplo dele é de: ${triplo}`);
