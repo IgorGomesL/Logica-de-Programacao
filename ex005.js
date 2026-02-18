7f9118088def6a8e4c1ecdf3e3c3508536e7bc79
Igor Gomes de Lima <gomesigor335@gmail.com>
2026-02-18 17:24:24 -0300

    Create ex005.js

---
+++ ex005.js
@@ -0,0 +1,7 @@
+//Converta reais para dólares (valor fixo definido no código).
+
+let dolar = 5.24
+let reais = 100
+let conversao = reais / dolar
+
+console.log(`O valor de ${reais} reais em dólares é de ${conversao.toFixed(2)} dólares.`)
