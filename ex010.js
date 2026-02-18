95838df7fafdb7fa3463efe17c9ffe2136a7c38d
Igor Gomes de Lima <gomesigor335@gmail.com>
2026-02-18 18:34:17 -0300

    Create ex010.js

---
+++ ex010.js
@@ -0,0 +1,6 @@
+// Converta horas em minutos e segundos.
+
+let horas = 14
+let minutos = horas * 60
+let segundos = minutos * 60
+console.log(`${horas} horas é equivalente a ${minutos} minutos ou ${segundos} segundos`)
