7c314d4e337cfdb9637255247bb5c752eec573c2
Igor Gomes de Lima <gomesigor335@gmail.com>
2026-02-18 18:04:21 -0300

    Create ex008.js

---
+++ ex008.js
@@ -0,0 +1,6 @@
+//Calcule o salário com aumento de 10%.
+
+let salario = 1500
+let aumento = salario / 10
+let salario_aumentado = salario + aumento
+console.log(`O seu salário era de ${salario} e recebeu um aumento de 10% e foi para ${salario_aumentado}`)
