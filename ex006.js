86fc9818492560736d7af08da5e420b065ea8e81
Igor Gomes de Lima <gomesigor335@gmail.com>
2026-02-18 17:29:31 -0300

    Create ex006.js

---
+++ ex006.js
@@ -0,0 +1,6 @@
+//Calcule a área de um retângulo.
+
+let altura = 10
+let base = 5
+let area = altura * base
+console.log(`A área de um retângulo com altura de ${altura}cm e base de ${base}cm é de ${area}cm²`)
